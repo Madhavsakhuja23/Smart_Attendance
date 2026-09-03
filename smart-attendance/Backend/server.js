5674f6db92f83a10ad5b09ad908d26349e7a16ff
Madhav26 <msakhuja22@gmail.com>
2026-09-03 16:33:24 +0530

    cors updated'

--- smart-attendance/Backend/server.js
+++ smart-attendance/Backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+
 const adminRoutes = require("./routes/adminRoutes");
 const authRoutes = require("./routes/authRoutes");
 const teacherRoutes = require("./routes/teacherRoutes");
@@ -11,27 +12,78 @@ dotenv.config();
 
 const app = express();
 
-// Connect MongoDB
 connectDB();
 
-// Middleware
-app.use(cors());
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://smart-attendance-cu.vercel.app"
+];
+
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+
+            if (!origin) {
+                return callback(null, true);
+            }
+
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+
+            return callback(
+                new Error("Not allowed by CORS")
+            );
+        },
+
+        methods: [
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "OPTIONS"
+        ],
+
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization"
+        ],
+
+        credentials: false
+    })
+);
+
+
 app.use(express.json());
 
-// Test route
+
 app.get("/", (req, res) => {
+
     res.json({
         status: "success",
         message: "Smart Attendance Backend is running"
     });
+
 });
 
-app.use("/api/admin", adminRoutes);
+
 app.use("/api/auth", authRoutes);
+
+app.use("/api/admin", adminRoutes);
+
 app.use("/api/teacher", teacherRoutes);
 
-const PORT = process.env.PORT || 5000;
+
+const PORT =
+    process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+
+    console.log(
+        `Server running on port ${PORT}`
+    );
+
 });
